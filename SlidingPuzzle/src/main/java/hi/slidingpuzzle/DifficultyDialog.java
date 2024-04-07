6828bcf693c63db1bc0ee601f4da74bf04154203
Glupakkur <hhernysson@gmail.com>
2024-04-07 15:33:51 +0000

    Bætti við DialogPane og handler til að velja erfiðleikastig

---
+++ SlidingPuzzle/src/main/java/hi/slidingpuzzle/DifficultyDialog.java
@@ -0,0 +1,66 @@
+package hi.slidingpuzzle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import java.io.IOException;
+import vinnsla.Difficulty;
+import java.util.Optional;
+import javafx.scene.control.Button;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.DialogPane;
+
+public class DifficultyDialog extends Dialog<Difficulty> {
+
+    Difficulty difficulty =new Difficulty(0);
+    @FXML
+    private ButtonType fxILagi;
+
+
+    public DifficultyDialog(){
+        setDialogPane(lesaDifficultyDialog());
+        iLagiRegla();
+        setResultConverter(b -> {
+            if (b.getButtonData() == ButtonBar.ButtonData.OK_DONE){
+                return difficulty;
+            } else {
+                return null;
+            }
+        });
+    }
+
+    private DialogPane lesaDifficultyDialog(){
+        FXMLLoader fxmlLoader = new FXMLLoader((getClass()).getResource("Difficulty-view.fxml"));
+        try{
+            fxmlLoader.setController(this);
+            return fxmlLoader.load();
+        } catch (IOException exception){
+            throw new RuntimeException(exception);
+        }
+    }
+    private void iLagiRegla() {
+        Node iLagi = getDialogPane().lookupButton(fxILagi);
+        iLagi.setDisable(true);
+
+    }
+    public Difficulty getDifficultyValue(){
+        Optional<Difficulty> utkoma = showAndWait();
+        return utkoma.orElse(null);
+    }
+    public void easy(ActionEvent actionEvent){
+        difficulty.setValue(1);
+        Node iLagi = getDialogPane().lookupButton(fxILagi);
+        iLagi.setDisable(false);
+    }
+    public void medium(ActionEvent actionEvent){
+        difficulty.setValue(2);
+        Node iLagi = getDialogPane().lookupButton(fxILagi);
+        iLagi.setDisable(false);
+    }
+    public void hard(ActionEvent actionEvent){
+        difficulty.setValue(3);
+        Node iLagi = getDialogPane().lookupButton(fxILagi);
+        iLagi.setDisable(false);
+    }
+}
---
+++ SlidingPuzzle/src/main/java/vinnsla/Difficulty.java
@@ -0,0 +1,18 @@
+package vinnsla;
+
+public class Difficulty {
+    private int value;
+
+    public Difficulty(int difficulty){
+        value = difficulty;
+    }
+    public int getValue(){
+        return value;
+    }
+
+    public void setValue(int difficulty){
+        this.value=difficulty;
+    }
+
+
+}
